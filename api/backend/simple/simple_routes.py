e7102d501fe4dbdb951b423b49d8359b454338b0
Alyssa Haidar <alyssa.haidar14@gmail.com>
2026-06-04 12:51:17 -0400

    fixed various issues with the model files and model cleaning

--- api/backend/simple/simple_routes.py
+++ api/backend/simple/simple_routes.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, jsonify, current_app, redirect, url_for
 from backend.simple.playlist import sample_playlist_data
-from backend.ml_models import model01
 
 # This blueprint handles basic routes useful for testing and demonstration
 simple_routes = Blueprint("simple_routes", __name__)
